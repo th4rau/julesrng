6da5f5946135596dd56ff50eeb6d702a0c31eaa7
th4rau <168271929+th4rau@users.noreply.github.com>
2025-04-11 23:31:02 +0300

    Create hi.lua

---
+++ hi.lua
@@ -0,0 +1,20 @@
+local Player = game.Players.LocalPlayer
+local Character = Player.Character
+
+Player.OnTeleport:Connect(function(State)
+	queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()")
+end)
+if game.PlaceId ~= 17297111758 then
+game:GetService("TeleportService"):Teleport(17297111758, Player)
+end
+local Player = game.Players.LocalPlayer
+local Character = Player.Character
+Character:PivotTo(workspace.Map.StartTimer.CFrame)
+task.wait(5)
+for i=1,5 do
+task.wait(0.6)
+Character:PivotTo(workspace.Map.Hitbox.CFrame)
+end
+Player:Kick("rejoining")
+task.wait(0.2)
+game:GetService("TeleportService"):Teleport(17297111758, Player)
